191bb1246b150ca5d89acb95328e20dc15d21592
Anastasiya Fomina <anastasiya.fomina@quantori.com>
2020-09-29 20:18:03 +0300

    BAP-33: remove extra import

--- src/Behavioral.Automation/Services/ListServices.cs
+++ src/Behavioral.Automation/Services/ListServices.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Behavioral.Automation.Elements;
 using TechTalk.SpecFlow;
 
