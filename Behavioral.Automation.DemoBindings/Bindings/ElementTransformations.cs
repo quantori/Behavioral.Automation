3be1966d244417df164db12a81df883a0160764e
Anastasiya Fomina <anastasiya.fomina@quantori.com>
2020-10-14 19:24:49 +0300

    BAP-44: add project for demo scenarios

---
+++ Behavioral.Automation.DemoBindings/Bindings/ElementTransformations.cs
@@ -0,0 +1,31 @@
+﻿using Behavioral.Automation.DemoBindings.Elements;
+using Behavioral.Automation.Elements;
+using Behavioral.Automation.Services;
+using JetBrains.Annotations;
+using TechTalk.SpecFlow;
+
+namespace Behavioral.Automation.DemoBindings.Bindings
+{
+    [Binding]
+    class ElementTransformations
+    {
+        private readonly IDriverService _driverService;
+        private readonly IElementSelectionService _selectionService;
+
+        public ElementTransformations(
+            [NotNull] IDriverService driverService,
+            [NotNull] IElementSelectionService selectionService)
+        {
+            _driverService = driverService;
+            _selectionService = selectionService;
+        }
+
+        [StepArgumentTransformation]
+        public IWebElementWrapper FindElement([NotNull] string caption)
+        {
+            return new WebElementWrapper(() => _selectionService.Find(caption),
+                caption,
+                _driverService);
+        }
+    }
+}
---
+++ Behavioral.Automation.DemoBindings/Bootstrapper.cs
@@ -0,0 +1,44 @@
+﻿using Behavioral.Automation.FluentAssertions;
+using Behavioral.Automation.Services;
+using BoDi;
+using TechTalk.SpecFlow;
+
+namespace Behavioral.Automation.DemoBindings
+{
+    [Binding]
+    public class Bootstrapper
+    {
+        private readonly IObjectContainer _objectContainer;
+        private readonly ITestRunner _runner;
+        private static DemoTestServicesBuilder _servicesBuilder;
+        private static readonly BrowserRunner BrowserRunner = new BrowserRunner();
+
+        public Bootstrapper(IObjectContainer objectContainer, ITestRunner runner)
+        {
+            _objectContainer = objectContainer;
+            _runner = runner;
+            _servicesBuilder = new DemoTestServicesBuilder(objectContainer, new TestServicesBuilder(_objectContainer));
+        }
+
+        [BeforeTestRun]
+        public static void OpenBrowser()
+        {
+            BrowserRunner.OpenChrome();
+        }
+
+        [AfterTestRun]
+        public static void CloseBrowser()
+        {
+            BrowserRunner.CloseBrowser();
+        }
+
+        [BeforeScenario(Order = 0)]
+        public void Bootstrap()
+        {
+            Assert.SetRunner(_runner);
+            _objectContainer.RegisterTypeAs<UserInterfaceBuilder, IUserInterfaceBuilder>();
+            _servicesBuilder.Build();
+            Assert.SetConsumer(_objectContainer.Resolve<IScenarioExecutionConsumer>());
+        }
+    }
+}
