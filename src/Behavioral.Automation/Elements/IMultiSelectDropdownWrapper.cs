72e1268535249a28a3712ee170611fe58007c573
Sergey Ruzin <sergey.ruzin@quantori.com>
2020-11-18 10:22:28 +0300

    Added wrapper and bindings for multiselect dropdown

---
+++ src/Behavioral.Automation/Elements/IMultiSelectDropdownWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+
+namespace Behavioral.Automation.Elements
+{
+    public interface IMultiSelectDropdownWrapper : IDropdownWrapper
+    {
+        IEnumerable<string> SelectedValuesTexts { [NotNull] get; }
+    }
+}
