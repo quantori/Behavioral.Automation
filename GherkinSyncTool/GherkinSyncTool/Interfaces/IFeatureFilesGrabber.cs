9c258a9ff766b97964be191b67aabd89a3b04349
Andrey Brovko <brov.ko@ya.ru>
2021-08-25 18:52:25 +0300

    Add interfaces summary

--- GherkinSyncTool/GherkinSyncTool/Interfaces/IFeatureFilesGrabber.cs
+++ GherkinSyncTool/GherkinSyncTool/Interfaces/IFeatureFilesGrabber.cs
@@ -2,8 +2,15 @@
 
 namespace GherkinSyncTool.Interfaces
 {
+    /// <summary>
+    /// The IFeatureFilesGrabber interface implements a method that takes feature files from a source
+    /// </summary>
     public interface IFeatureFilesGrabber
     {
+        /// <summary>
+        /// Takes feature-files from a source
+        /// </summary>
+        /// <returns>List of a feature files</returns>
         List<IFeatureFile> TakeFiles();
     }
 }
