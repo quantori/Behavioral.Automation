9414aee22fc87b73ffa2dcb0abfa8519d2aba946
Vladimir Semenovich <vladimir.semenovich@quantori.com>
2021-08-26 17:35:27 +0300

    refactor: testrail version change, TestRailNoCaseException.cs parent class changed to TestRailException

--- GherkinSyncTool/GherkinSyncTool/Exceptions/TestRailNoCaseException.cs
+++ GherkinSyncTool/GherkinSyncTool/Exceptions/TestRailNoCaseException.cs
@@ -5,7 +5,7 @@ namespace GherkinSyncTool.Exceptions
     /// <summary>
     /// Thrown when TestRail does not have a test case with provided Id
     /// </summary>
-    public class TestRailNoCaseException : Exception
+    public class TestRailNoCaseException : TestRailException
     {
         public TestRailNoCaseException(string message, Exception innerException) : base(message, innerException)
         {
