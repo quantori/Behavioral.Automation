84ced990b56205e753afae59713f06e999161a61
Alexander Pozdnyakov <alexander.pozdnyakov@quantori.com>
2022-01-21 15:05:43 +0300

    adding a new demo application

--- Behavioral.Automation.DemoBindings/Bootstrapper.cs
+++ Behavioral.Automation.DemoBindings/Bootstrapper.cs
@@ -1,4 +1,8 @@
-﻿using Behavioral.Automation.FluentAssertions;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using Behavioral.Automation.FluentAssertions;
 using Behavioral.Automation.Services;
 using BoDi;
 using TechTalk.SpecFlow;
@@ -12,6 +16,8 @@ namespace Behavioral.Automation.DemoBindings
         private readonly ITestRunner _runner;
         private readonly DemoTestServicesBuilder _servicesBuilder;
         private readonly BrowserRunner _browserRunner;
+        private static Process _coreRunProcess;
+        private const string BaseUrl = "http://localhost:4200";
 
         public Bootstrapper(IObjectContainer objectContainer, ITestRunner runner, BrowserRunner browserRunner)
         {
@@ -20,11 +26,61 @@ namespace Behavioral.Automation.DemoBindings
             _browserRunner = browserRunner;
             _servicesBuilder = new DemoTestServicesBuilder(objectContainer, new TestServicesBuilder(_objectContainer));
         }
+        
+        private static void SetUpTestApp()
+        {
+            try
+            {
+                PingTestApp();
+            }
+            catch (WebException)
+            {
+                RunTestApp();
+            }
+        }
+
+        private static WebResponse PingTestApp() =>
+            WebRequest.CreateHttp(BaseUrl).GetResponse();
+
+        private static void RunTestApp()
+        {
+            string testAppPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "..", "src",
+                "BlazorApp");
+
+            _coreRunProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/c dotnet run",
+                    WorkingDirectory = testAppPath
+                }
+            };
+            _coreRunProcess.Start();
+        }
+
+        [BeforeTestRun]
+        public static void StartDemoApp()
+        {
+            SetUpTestApp();
+        }
+
+        [AfterTestRun]
+        public static void StopDemoApp()
+        {
+            if (_coreRunProcess != null)
+            {
+                _coreRunProcess.Kill(true);
+                _coreRunProcess.Dispose();
+            }
+        }
+        
 
         [AfterScenario]
         public void CloseBrowser()
         {
             _browserRunner.CloseBrowser();
+            
         }
 
         [BeforeScenario(Order = 0)]
--- src/App/src/automaiton/001-LabelBinding.feature
+++
@@ -1,7 +0,0 @@
-Feature: 001 Label Binding
-
-  @Automated
-  @NoAuthentication
-  Scenario: 001-01 User goes to main page
-    Then "Simple text" is visible
-    And "Simple text" label text should become "Behavioral automation demo"
