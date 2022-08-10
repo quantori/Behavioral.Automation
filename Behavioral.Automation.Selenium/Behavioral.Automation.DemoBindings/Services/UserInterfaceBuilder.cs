53857521ebfdb3692077bd337a6299df8d5c8dae
Vladimir Semenovich <vladimir.semenovich@quantori.com>
2022-08-10 12:19:56 +0300

    feat: id changes

--- Behavioral.Automation.Selenium/Behavioral.Automation.DemoBindings/Services/UserInterfaceBuilder.cs
+++ Behavioral.Automation.Selenium/Behavioral.Automation.DemoBindings/Services/UserInterfaceBuilder.cs
@@ -3,7 +3,7 @@ using Behavioral.Automation.Services.Mapping.Contract;
 
 namespace Behavioral.Automation.DemoBindings
 {
-    public class UserInterfaceBuilder : UserInterfaceBuilderBase
+    public partial class UserInterfaceBuilder : UserInterfaceBuilderBase
     {
         public UserInterfaceBuilder(IScopeMarkupMapper mapper)
             : base(mapper)
@@ -15,8 +15,34 @@ namespace Behavioral.Automation.DemoBindings
         {
             using (var mappingPipe = Mapper.GetGlobalMappingPipe())
             {
-                mappingPipe.Register("label").Alias("label")
-                    .With("main-page-header").As("Demo");
+                mappingPipe.Register("header").Alias("header")
+                    .With("main-page-header").As("Main page")
+                    .With("main-elements-list-header").As("Elements list")
+                    .With("greetings-page-header").As("Greetings page")
+                    .With("counter-page-header").As("Counter page")
+                    .With("forecast-page-header").As("Weather forecast page");
+
+                mappingPipe.Register("text").Alias("text")
+                    .With("main-page-paragraph").As("Main page")
+                    .With("greetings-input-text").As("Enter your name")
+                    .With("counter-page-text").As("Current count")
+                    .With("forecast-page-paragraph").As("Forecast page");
+
+                mappingPipe.Register("table").Alias("table")
+                    .With("forecast-table").As("Forecast");
+
+                mappingPipe.Register("link").Alias("link")
+                    .With("nav-link-home").As("Homepage")
+                    .With("nav-link-greetings").As("Greetings")
+                    .With("nav-link-counter").As("Counter")
+                    .With("nav-link-forecast").As("Forecast");
+
+                mappingPipe.Register("button").Alias("button")
+                    .With("greetings-page-button").As("Say hello")
+                    .With("counter-page-button").As("Click me");
+
+                mappingPipe.Register("field").Alias("field")
+                    .With("greetings-page-input").As("Name");
             }
         }
     }
