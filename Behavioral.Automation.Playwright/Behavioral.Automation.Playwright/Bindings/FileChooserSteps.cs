21aa69460c6f43c5ca9fc42ae4f860a3cfcb40fe
Oleg Mozhey <oleg.mozhey@quantori.com>
2023-08-07 16:40:46 +0400

    preliminary

---
+++ Behavioral.Automation.Playwright/Behavioral.Automation.Playwright/Bindings/FileChooserSteps.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Behavioral.Automation.Playwright.Context;
+using Behavioral.Automation.Playwright.WebElementsWrappers.Interface;
+using TechTalk.SpecFlow;
+
+namespace Behavioral.Automation.Playwright.Bindings;
+
+[Binding]
+public class FileChooserSteps
+{
+    private readonly WebContext context;
+
+    public FileChooserSteps(WebContext context)
+    {
+        this.context = context;
+    }
+
+    [Then(@"file chooser should become visible")]
+    public async Task ThenFileChooserShouldBecomeVisible()
+    {
+        await context.Page.FileChooser
+    }
+}
