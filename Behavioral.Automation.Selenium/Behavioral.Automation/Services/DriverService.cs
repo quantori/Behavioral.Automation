98ded4b42f0e042c1140e129f4d842790ffbfa40
OlegMozhey <oleg.mozhey@quantori.com>
2024-02-27 13:51:38 +0400

    rollback changes

--- Behavioral.Automation.Selenium/Behavioral.Automation/Services/DriverService.cs
+++ Behavioral.Automation.Selenium/Behavioral.Automation/Services/DriverService.cs
@@ -269,8 +269,8 @@ namespace Behavioral.Automation.Services
             var fileName = new string(TestContext.CurrentContext.Test.Name
                 .Where(x => !Path.GetInvalidFileNameChars().Contains(x))
                 .ToArray()) + ".png";
-            var screenshot = ((ITakesScreenshot)Driver).GetScreenshot();
-            screenshot.SaveAsFile(fileName);
+            Screenshot screenshot = ((ITakesScreenshot)Driver).GetScreenshot();
+            screenshot.SaveAsFile(fileName, ScreenshotImageFormat.Png);
             return fileName;
         }
 
