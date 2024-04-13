0546bc14b848226f598d17af272703f80e4363c3
OlegMozhey <oleg.mozhey@quantori.com>
2024-04-14 00:17:14 +0400

    small changes

--- Behavioral.Automation.Implementation.Selenium/Behavioral.Automation.Implementation.Selenium/Bindings/BrowserRunner.cs
+++ Behavioral.Automation.Implementation.Selenium/Behavioral.Automation.Implementation.Selenium/Bindings/BrowserRunner.cs
@@ -1,104 +1,112 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using Behavioral.Automation.Configs;
+using Behavioral.Automation.Implementation.Selenium.Configs;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace Behavioral.Automation.Implementation.Selenium.Bindings;
 
 public class BrowserRunner
+{
+    public IWebDriver Driver;
+
+    /// <summary>
+    /// Driver initialization
+    /// </summary>
+    /// <param name="driver">IWebDriver object</param>
+    public void OpenBrowser([NotNull] IWebDriver driver)
     {
-        public IWebDriver Driver;
+        Driver = driver;
+    }
 
-        /// <summary>
-        /// Driver initialization
-        /// </summary>
-        /// <param name="driver">IWebDriver object</param>
-        public void OpenBrowser([NotNull] IWebDriver driver)
+
+    /// <summary>
+    /// Driver disposal
+    /// </summary>
+    public void CloseBrowser()
+    {
+        Driver.Dispose();
+    }
+
+    /// <summary>
+    /// Open and configure Google Chrome browser
+    /// </summary>
+    /// <param name="options">Browser parameters</param>
+    public void OpenChrome(ChromeOptions options = null)
+    {
+        var downloadPath = ConfigManager.GetConfig<Config>().DownloadPath ?? Environment.CurrentDirectory;
+
+        if (options == null)
         {
-            Driver = driver;
-        }
-
-
-        /// <summary>
-        /// Driver disposal
-        /// </summary>
-        public void CloseBrowser()
-        {
-            Driver.Dispose();
-        }
-
-        /// <summary>
-        /// Open and configure Google Chrome browser
-        /// </summary>
-        /// <param name="options">Browser parameters</param>
-        public void OpenChrome(ChromeOptions options = null)
-        {
-            var downloadPath = ConfigServiceBase.DownloadPath ?? Environment.CurrentDirectory;
-
-            if (options == null)
+            options = new ChromeOptions();
+            options.AddArguments(new List<string>(
+                ConfigManager.GetConfig<Config>().BrowserParameters.Split(" ", StringSplitOptions.RemoveEmptyEntries)));
+            if (ConfigManager.GetConfig<Config>().BrowserParameters.Contains("headless"))
             {
-                options = new ChromeOptions();
-                options.AddArguments(new List<string>(
-                   ConfigServiceBase.BrowserParameters.Split(" ", StringSplitOptions.RemoveEmptyEntries)));
-                if (ConfigServiceBase.BrowserParameters.Contains("headless"))
-                {
-                    ConfigureChromeHeadlessDownload(options, downloadPath);
-                }
-                if (ConfigServiceBase.AccessClipboard)
-                {
-                    ConfigureClipboardAccess(options);
-                }
-                options.AddUserProfilePreference("intl.accept_languages", "en,en_US");
-                options.AcceptInsecureCertificates = ConfigServiceBase.AcceptInsecureCertificates;
-                options.SetLoggingPreference(LogType.Browser, LogLevel.All);
-                if (!string.IsNullOrWhiteSpace(ConfigServiceBase.BrowserBinaryLocation))
-                {
-                    options.BinaryLocation = Environment.ExpandEnvironmentVariables(ConfigServiceBase.BrowserBinaryLocation);
-                }
-                if (!string.IsNullOrWhiteSpace(ConfigServiceBase.UnhandledPromptBehavior))
-                {
-                    options.UnhandledPromptBehavior = ConfigServiceBase.UnhandledPromptBehavior switch
-                    {
-                        "Accept" => UnhandledPromptBehavior.Accept,
-                        "Dismiss" => UnhandledPromptBehavior.Dismiss,
-                        "Ignore" => UnhandledPromptBehavior.Ignore,
-                        _ => options.UnhandledPromptBehavior
-                    };
-                }
+                ConfigureChromeHeadlessDownload(options, downloadPath);
             }
 
-            var driver = new ChromeDriver(options);
-            var param = new Dictionary<string, object>
+            if (ConfigManager.GetConfig<Config>().AccessClipboard)
             {
-                {"behavior", "allow"},
-                {"downloadPath", downloadPath}
-            };
-            driver.ExecuteCdpCommand("Page.setDownloadBehavior", param);
-            OpenBrowser(driver);
+                ConfigureClipboardAccess(options);
+            }
+
+            options.AddUserProfilePreference("intl.accept_languages", "en,en_US");
+            options.AcceptInsecureCertificates = ConfigServiceBase.AcceptInsecureCertificates;
+            options.SetLoggingPreference(LogType.Browser, LogLevel.All);
+            if (!string.IsNullOrWhiteSpace(ConfigServiceBase.BrowserBinaryLocation))
+            {
+                options.BinaryLocation =
+                    Environment.ExpandEnvironmentVariables(ConfigServiceBase.BrowserBinaryLocation);
+            }
+
+            if (!string.IsNullOrWhiteSpace(ConfigServiceBase.UnhandledPromptBehavior))
+            {
+                options.UnhandledPromptBehavior = ConfigServiceBase.UnhandledPromptBehavior switch
+                {
+                    "Accept" => UnhandledPromptBehavior.Accept,
+                    "Dismiss" => UnhandledPromptBehavior.Dismiss,
+                    "Ignore" => UnhandledPromptBehavior.Ignore,
+                    _ => options.UnhandledPromptBehavior
+                };
+            }
         }
 
-        /// <summary>
-        /// Configure Google Chrome downloads to work correctly in headless mode
-        /// </summary>
-        /// <param name="options">Chrome configuration parameters</param>
-        /// <param name="downloadPath">Directory to download files to</param>
-        private void ConfigureChromeHeadlessDownload(ChromeOptions options, string downloadPath)
+        var driver = new ChromeDriver(options);
+        var param = new Dictionary<string, object>
         {
-            options.AddUserProfilePreference("download.prompt_for_download", "false");
-            options.AddUserProfilePreference("download.directory_upgrade", "true");
-            options.AddUserProfilePreference("download.default_directory", downloadPath);
-        }
+            {"behavior", "allow"},
+            {"downloadPath", downloadPath}
+        };
+        driver.ExecuteCdpCommand("Page.setDownloadBehavior", param);
+        OpenBrowser(driver);
+    }
 
-        private void ConfigureClipboardAccess(ChromeOptions options)
+    /// <summary>
+    /// Configure Google Chrome downloads to work correctly in headless mode
+    /// </summary>
+    /// <param name="options">Chrome configuration parameters</param>
+    /// <param name="downloadPath">Directory to download files to</param>
+    private void ConfigureChromeHeadlessDownload(ChromeOptions options, string downloadPath)
+    {
+        options.AddUserProfilePreference("download.prompt_for_download", "false");
+        options.AddUserProfilePreference("download.directory_upgrade", "true");
+        options.AddUserProfilePreference("download.default_directory", downloadPath);
+    }
+
+    private void ConfigureClipboardAccess(ChromeOptions options)
+    {
+        var clipboardExceptionSettings = new Dictionary<string, object>
         {
-            var clipboardExceptionSettings = new Dictionary<string, object> {
-                {ConfigServiceBase.BaseUrl,
-                    new Dictionary<string, long> {
-                        {"last_modified", DateTimeOffset.Now.ToUnixTimeMilliseconds()},
-                        {"setting", 1}
-                    }
+            {
+                ConfigServiceBase.BaseUrl,
+                new Dictionary<string, long>
+                {
+                    {"last_modified", DateTimeOffset.Now.ToUnixTimeMilliseconds()},
+                    {"setting", 1}
                 }
-            };
-            options.AddUserProfilePreference("profile.content_settings.exceptions.clipboard", clipboardExceptionSettings);
-        }
+            }
+        };
+        options.AddUserProfilePreference("profile.content_settings.exceptions.clipboard", clipboardExceptionSettings);
     }
 }
--- Behavioral.Automation.Implementation.Selenium/Behavioral.Automation.Implementation.Selenium/Configs/Config.cs
+++ Behavioral.Automation.Implementation.Selenium/Behavioral.Automation.Implementation.Selenium/Configs/Config.cs
@@ -4,9 +4,29 @@ namespace Behavioral.Automation.Implementation.Selenium.Configs;
 
 public class Config
 {
-    [ConfigurationKeyName("BASE_URL")] 
-    public string BaseUrl { get; set; }
-    
-    [ConfigurationKeyName("SEARCH_ATTRIBUTE")] 
+    [ConfigurationKeyName("BASE_URL")] public string BaseUrl { get; set; }
+
+    [ConfigurationKeyName("BROWSER_PARAMS")]
+    public string BrowserParameters { get; set; }
+
+    [ConfigurationKeyName("DOWNLOAD_PATH")]
+    public string DownloadPath { get; set; } = AppContext.BaseDirectory;
+
+    [ConfigurationKeyName("ACCEPT_INSECURE_CERTIFICATES")]
+    public bool AcceptInsecureCertificates { get; set; } = true;
+
+    [ConfigurationKeyName("SEARCH_ATTRIBUTE")]
     public string SearchAttribute { get; set; }
+
+    [ConfigurationKeyName("ACCESS_CLIPBOARD")]
+    public bool AccessClipboard { get; set; } = false;
+
+    [ConfigurationKeyName("BROWSER_BINARY_LOCATION")]
+    public string BrowserBinaryLocation { get; set; }
+
+    [ConfigurationKeyName("UNHANDLED_PROMPT_BEHAVIOR")]
+    public string UnhandledPromptBehavior { get; set; }
+
+    [ConfigurationKeyName("ASSERT_ATTEMPTS")]
+    public int? AssertAttempts { get; set; } = 30;
 }
