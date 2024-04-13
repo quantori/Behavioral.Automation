d30a9c44e151386062deffa07e33528c2d9cbd0c
OlegMozhey <oleg.mozhey@quantori.com>
2024-04-13 22:38:48 +0400

    simplify dependepcies

--- Behavioral.Automation.Implementation.Selenium/Behavioral.Automation.Implementation.Selenium/Bindings/TestClass.cs
+++ Behavioral.Automation.Implementation.Selenium/Behavioral.Automation.Implementation.Selenium/Bindings/TestClass.cs
@@ -1,6 +1,21 @@
-﻿namespace Behavioral.Automation.Implementation.Selenium.Bindings;
+﻿using Behavioral.Automation.AsyncAbstractions.UI.Interfaces;
 
-public class TestClass
+namespace Behavioral.Automation.Implementation.Selenium.Bindings;
+
+public class TestClass : IPage
 {
-    
+    public Task GoToUrlAsync(string url)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task GoToApplicationUrlAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task HaveTitleAsync(string title)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Behavioral.Automation.Implementation.Selenium/UITests/Features/Tests.feature
+++ Behavioral.Automation.Implementation.Selenium/UITests/Features/Tests.feature
@@ -1,5 +1,6 @@
 ﻿Feature: Tests for Bahavioral.Automation.Playwright project
 
   Scenario: Button click binding test
+    
     Given application URL was opened
     When user clicks the "test" button
