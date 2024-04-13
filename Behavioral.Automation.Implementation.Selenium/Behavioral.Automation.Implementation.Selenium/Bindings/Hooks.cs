fa93ac50c8e51be4c58dbf738583c51e0693a426
OlegMozhey <oleg.mozhey@quantori.com>
2024-04-13 23:58:37 +0400

    Added configs to selenium project

---
+++ Behavioral.Automation.Implementation.Selenium/Behavioral.Automation.Implementation.Selenium/Bindings/Hooks.cs
@@ -0,0 +1,89 @@
+﻿using System.Diagnostics;
+using System.Net;
+using Behavioral.Automation.Configs;
+using Behavioral.Automation.Implementation.Selenium.Configs;
+using BoDi;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Behavioral.Automation.Implementation.Selenium.Bindings;
+
+[Binding]
+public class Hooks
+{
+        private readonly IObjectContainer _objectContainer;
+        private readonly ITestRunner _runner;
+        private readonly BrowserRunner _browserRunner;
+        private static Process _coreRunProcess;
+        
+        public Hooks(IObjectContainer objectContainer, ITestRunner runner, BrowserRunner browserRunner)
+        {
+            _objectContainer = objectContainer;
+            _runner = runner;
+            _browserRunner = browserRunner;
+        }
+
+        private static bool IsConnectionEstablished()
+        {
+            try
+            {
+                WebRequest.CreateHttp(ConfigManager.GetConfig<Config>().BaseUrl).GetResponse();
+                return true;
+            }
+            catch (WebException)
+            {
+                return false;
+            }
+        }
+
+        private static void RunTestApp()
+        {
+            string testAppPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "..", "..", "src",
+                "BlazorApp");
+
+            _coreRunProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/c dotnet run",
+                    WorkingDirectory = testAppPath
+                }
+            };
+            _coreRunProcess.Start();
+        }
+
+        [BeforeTestRun]
+        public static void StartDemoApp()
+        {
+            if (!IsConnectionEstablished())
+                RunTestApp();
+        }
+
+        [AfterTestRun]
+        public static void StopDemoApp()
+        {
+            if (_coreRunProcess != null)
+            {
+                _coreRunProcess.Kill(true);
+                _coreRunProcess.Dispose();
+            }
+        }
+
+        [AfterScenario]
+        public void CloseBrowser()
+        {
+            _browserRunner.CloseBrowser();
+            
+        }
+
+        [BeforeScenario(Order = 0)]
+        public void Bootstrap()
+        {
+            Assert.SetRunner(_runner);
+            _objectContainer.RegisterTypeAs<UserInterfaceBuilder, IUserInterfaceBuilder>();
+            _servicesBuilder.Build();
+            _browserRunner.OpenChrome();
+        }
+    }
+}
---
+++ Behavioral.Automation.Implementation.Selenium/Behavioral.Automation.Implementation.Selenium/Configs/Config.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Behavioral.Automation.Implementation.Selenium.Configs;
+
+public class Config
+{
+    [ConfigurationKeyName("BASE_URL")] 
+    public string BaseUrl { get; set; }
+    
+    [ConfigurationKeyName("SEARCH_ATTRIBUTE")] 
+    public string SearchAttribute { get; set; }
+}
