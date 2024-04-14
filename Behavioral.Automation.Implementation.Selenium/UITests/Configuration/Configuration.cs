3b9d6d5afa22191c1318a607be75db0d833055c1
OlegMozhey <oleg.mozhey@quantori.com>
2024-04-14 16:44:52 +0400

    added first configuration of web elements

--- Behavioral.Automation.Implementation.Selenium/UITests/Configuration/Configuration.cs
+++ Behavioral.Automation.Implementation.Selenium/UITests/Configuration/Configuration.cs
@@ -1,7 +1,15 @@
-﻿namespace UITests.Configuration;
+﻿using Behavioral.Automation.AsyncAbstractions.UI.Interfaces;
+using Behavioral.Automation.Implementation.Selenium.WebElements;
+
+namespace UITests.Configuration;
 
 [Binding]
 public class Configuration
 {
-    
+    // Configuration of DI and Factories should be done with order 0
+    [BeforeTestRun(Order = 0)]
+    public static void ConfigureUiImplementations()
+    {
+        IWebElementStorageService.RegisterWebElementImplementationAs<ButtonElement, IButtonElement>();
+    }
 }
